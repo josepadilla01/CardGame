a82a4cc80225c26321488274e5e07a68b37e5dae
josepadilla01 <wichopadillan@hotmail.com>
2017-10-27 14:53:53 -0500

    update

--- CardGame/Program.cs
+++ CardGame/Program.cs
@@ -12,38 +12,13 @@ namespace CardGame
     {
         private Deck mainDeck;
         private List<Card> discardPile;
-        private Hand p1_hand;
-        private Hand p2_hand;
+
+        private Player[] players;
+        private Hand[] pHands;
+
+
         int NUM_CARDS;
-        Player[] players = new Player[2];
-
-        Card cardTest;
-        //declare player variables
-
-        ////Grid
-        //for (int i = 0; i< 9; i++)
-        //{
-        //	if (i == 3 || i == 6)
-        //	{
-        //		Console.Write("\n");
-        //	}
-        //             p1_hand.Add(mainDeck.Draw());
-        //	Console.Write("{0}{1}  ", p1_hand.Peek(i).Suit, p1_hand.Peek(i).Value);
-        //}
-
-        //            Console.WriteLine("\n");
-
-
-        //for (int i = 0; i< 9; i++)
-        //{
-        //	if (i == 3 || i == 6)
-        //	{
-        //		Console.Write("\n");
-        //	}
-        //	p2_hand.Add(mainDeck.Draw());
-        //	Console.Write("{0}{1}  ", p2_hand.Peek(i).Suit, p2_hand.Peek(i).Value);
-        //}
-
+        Random rnd = new Random();
         //==========================================GAME CLASS====================================//
         public Game()
         {
@@ -62,18 +37,65 @@ namespace CardGame
 
             //get the right player
             int count = 0;
-            int playerChoice;
+            int playerChoice = 0;
 
             //Add a card to the discard pile to initialize it.
             //Set its faceup value to true.  NOTE: All cards in discard pile
             //are face up.
+            discardPile.Add(mainDeck.Draw());
 
-            //Set up logic to turn 3 cards face up per player
-
-            while (Turn())
+            //Set up logic to turn 3 random cards face up per player
+            int randomPosition;
+            for(int i = 0; i < 3; i++)
             {
+                randomPosition = rnd.Next(0, 9);
+
+                //Loops keep checking the player hand until it finds a facedown.
+                //Prevents from trying to change cards that are already face up.
+                while (true)
+                {
+                    if (pHands[0].Peek(randomPosition).FaceUp == false)
+                    {
+                        pHands[0].Peek(randomPosition).FaceUp = true;
+                        break;
+                    }
+                    randomPosition = rnd.Next(0, 9);
+                }
+
+                while(true)
+                {
+                    if (pHands[1].Peek(randomPosition).FaceUp == false)
+                    {
+                        pHands[1].Peek(randomPosition).FaceUp = true;
+                        break;
+                    }
+                }
+
                 
             }
+
+            while (Turn(players[playerChoice], pHands[playerChoice]))
+            {
+                Console.WriteLine("\n");
+
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (i == 3 || i == 6)
+                    {
+                        Console.Write("\n");
+                    }
+                    Console.Write("{0}{1}{2}  ", pHands[playerChoice].Peek(i).Suit, pHands[playerChoice].Peek(i).Value, pHands[playerChoice].Peek(i).FaceUp);
+                }
+
+                count++;
+                playerChoice = count % 2;
+            }
+
+            //Get the score for each hand
+            Console.WriteLine("\n");
+            Console.WriteLine("Player 1 score: {0}", pHands[0].Score());
+            Console.WriteLine("Player 2 score: {0}", pHands[1].Score());
 
         }
 
@@ -82,14 +104,18 @@ namespace CardGame
             //Setup all objects here
             mainDeck = new Deck();
             discardPile = new List<Card>();
-            p1_hand = new Hand();
-            p2_hand = new Hand();
-            players[0] = new Player(p1_hand);
-            players[1] = new Player(p2_hand);
+            players = new Player[2];
+            pHands = new Hand[2];
+            players[0] = new Player();
+            players[1] = new Player();
+            pHands[0] = new Hand();
+            pHands[1] = new Hand();
+             
         }
 
         private void Deal()
         {
+            Card next;
             NUM_CARDS = 9;
             //for num cards
             for (int i = 0; i < NUM_CARDS; i++)
@@ -98,22 +124,24 @@ namespace CardGame
                 for (int j = 0; j < 2; j++)
                 {
                     //draw from deck
-                    Card next = mainDeck.Draw();
-                    //add to hand
-                    p1_hand.Add(next);
                     next = mainDeck.Draw();
-                    p2_hand.Add(next);
+                    //add to hand
+                    pHands[j].Add(next);
+                   
                 }
             }
         }
 
-        public bool Turn(Player player)
+        public bool Turn(Player player, Hand playerHand)
         {
-            bool flag = true;
             int cardPosition;
             Card drawnCard;
-            
+            Card discardedCard;
             //Choose whether to draw from main deck or discard pile
+
+            if (playerHand.IsDone() == true)
+                return false;
+
             if (player.ChooseDrawDiscard() == true)
             {
                 drawnCard = mainDeck.Draw();
@@ -134,7 +162,7 @@ namespace CardGame
                     discardPile.RemoveAt(0);
                 }
             }
-
+            
             /*
              * Look at the drawn card and replace any card(face down or face up) in the hand. 
              * The new card is face up, the replaced card is added to
@@ -142,11 +170,20 @@ namespace CardGame
             */
 
             //get the card position first
-            cardPosition = player.ChooseReplace();
+            cardPosition = player.ChooseReplace(playerHand);
+
             //replace card at "cardPosition" with the card that was drawn
-            player
-            
-            
+            //store the card that was replaced
+            discardedCard = playerHand.Replace(cardPosition, drawnCard);
+
+            //Set the replaced card face up into the discard pile
+            if (discardedCard.FaceUp == false)
+            {
+                discardedCard.FaceUp = true;
+                discardPile.Add(discardedCard);
+            }
+            else
+                discardPile.Add(discardedCard);
 
             return true;
         }
@@ -157,9 +194,9 @@ namespace CardGame
     {
         Random rnd = new Random();
 
-        public Player(Hand playerHand)
+        public Player()
         {
-     
+            
         }
 
         //Chooses to either draw from the deck (true)
@@ -168,19 +205,25 @@ namespace CardGame
         {
             int probability = rnd.Next(0, 100);
 
-            if (probability <= 65)
+            if (probability <= 75)
+            {
+                Console.WriteLine("Choosing from main deck");
                 return true;
+            }
             else
+            {
+                Console.WriteLine("Choosing from discard");
                 return false;
+            }
         }
 
 
-        public int ChooseReplace()
+        public int ChooseReplace(Hand playerHand)
         {
             //keeps track of whether to choose a faceup or facedown
             bool cardFace = ChooseFaceUpOrDown();
 
-            int cardPosition = ChooseCard(cardFace);
+            int cardPosition = ChooseCard(cardFace, playerHand);
 
             return cardPosition;
 
@@ -205,26 +248,36 @@ namespace CardGame
             return face;
         }
 
+        
         public int ChooseCard(bool cardFace, Hand playerHand)
         {
-            
-            int cardPosition = rnd.Next(0,9);
-
+            int cardPosition = rnd.Next(0, 9);
+            Card c;
             //search through the hand for a card that is either facedown or faceup,
             //depending on the value of "cardFace"
-           while(true)
+
+
+            while (true)
             {
+                c = playerHand.Peek(cardPosition);
                 //look at a random position in the hand
-                if (hand.Peek(cardPosition).FaceUp == cardFace)
+                //If the boolean FaceUp value of the card in the hand matches the 
+                //desired card value(faceup or facedown)
+                //then we just return the position
+                
+                if (c.FaceUp == cardFace)
                 {
                     return cardPosition;
                 }
                 else
+                    //If the FaceUp value of the card does not match
+                    //the desired value (randomized), then just assign another random
+                    //card position
                     cardPosition = rnd.Next(0, 9);
             }
 
         }
-    
+
     }
 
     //=========================================HAND CLASS==============================================//
@@ -275,6 +328,96 @@ namespace CardGame
             return card_out;
         }
 
+        public bool IsDone()
+        {
+            //Go through all cards
+            //If they're all faceup, return true
+            for (int i = 0; i < 9; i++)
+            {
+                if (hand[i].FaceUp == false)
+                    return false;
+            }
+            return true;
+        }
+
+        public int Score()
+        {
+            //loop through the hand
+            //Check for duplicates on rows
+            for (int j = 0; j < 7; j += 3)
+            {
+                
+                //if all 3 cards are equal on each row, switch their
+                //suit value to Kings.  Will loop again through the whole hand
+                //to check for Kings and when calculating the score
+                if (hand[j] == hand[j + 1] && hand[j + 1] == hand[j + 2])
+                {
+                    hand[j].Suit = 'K';
+                    hand[j + 1].Suit = 'K';
+                    hand[j + 2].Suit = 'K';
+                }
+            }
+
+            //Check for duplicates on columns
+            for (int j = 0; j < 3; j++)
+            {
+                if (hand[j] == hand[j + 3] && hand[j + 3] == hand[j + 6])
+                {
+                    hand[j].Suit = 'K';
+                    hand[j + 3].Suit = 'K';
+                    hand[j + 6].Suit = 'K';
+                }
+            }
+
+            //Check for duplicates on row
+            for (int i = 0; i < 3; i++)
+            {
+                //When i = 0 or i = 2, check diagonally for duplicates
+                if (i == 0)
+                {
+                    if (hand[i] == hand[i + 4] && hand[i + 4] == hand[i + 8])
+                    {
+                        hand[i].Suit = 'K';
+                        hand[i + 4].Suit = 'K';
+                        hand[i + 8].Suit = 'K';
+                    }
+                }
+                else if(i == 2)
+                {
+                    if (hand[i] == hand[i + 2] && hand[i + 2] == hand[i + 4])
+                    {
+                        hand[i].Suit = 'K';
+                        hand[i + 2].Suit = 'K';
+                        hand[i + 4].Suit = 'K';
+                    }
+                }
+            }
+
+            //Now loop through the hand to calculate score.
+            //Ignore all Kings.
+            //Jokers subtract 2 from score
+            //J and Q are worth 10
+            //All else are worth their face value
+            int score = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (hand[i].Value > 1 && hand[i].Value < 11)
+                {
+                    score += hand[i].Value;
+                }
+                else if (hand[i].Value > 10 && hand[i].Value < 13)
+                {
+                    score += 10;
+                }
+                else if (hand[i].Suit == 'J')
+                {
+                    score -= 2;
+                }
+            }
+
+            return score;
+        }
+
     }
 
     //=========================================CARD CLASS==============================================//
@@ -311,7 +454,7 @@ namespace CardGame
             foreach (char suit in SUITS)
             {
                 //Ace card first
-                //deck.Add(new Card(1, suit));
+                deck.Add(new Card(1, 'A'));
                 for (int i = MAX_VALUE; i > MIN_VALUE; i--)
                 {
                     deck.Add(new Card(i, suit));
